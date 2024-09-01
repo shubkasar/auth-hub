468b1ca9d3cb8797858c492c7f7a2159e11e6194
shubkasar <shubhamk2409@gmail.com>
2024-09-01 18:11:43 -0400

    setup Middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+const authenticateToken = (req, res, next) => {
+    const token = req.header('Authorization');
+    if(!token){
+        return res.status(401).json({success: false, message: 'Access Denied'});
+    }
+    try {
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = verified;
+        next();
+    } catch (err) {
+        res.status(400).json({success: false, message: 'Invalid Token'});
+    }
+};
+
+module.exports = authenticateToken;
